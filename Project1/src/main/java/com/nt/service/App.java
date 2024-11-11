9b8e86e409baf62660ceeec606fd43333a677a51
HP <HP@DESKTOP-0MDEC3R>
2024-11-11 11:42:41 +0530

    Project created by TL

---
+++ Project1/src/main/java/com/nt/service/App.java
@@ -0,0 +1,15 @@
+package com.nt.service;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        App a = new App();
+        System.out.println("Sum : "+a.sum(10, 20));
+    }
+    public int  sum(int x, int y) {
+    	return x=+y;
+    }
+}

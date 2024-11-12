daaa74185f5ece5a684085580cc85b6fee37f121
HP <HP@DESKTOP-0MDEC3R>
2024-11-12 12:39:30 +0530

    completed

--- Project1/src/main/java/com/nt/upi/UPIPayment.java
+++ Project1/src/main/java/com/nt/upi/UPIPayment.java
@@ -1,8 +1,16 @@
 package com.nt.upi;
 
+import java.util.Random;
+
 public class UPIPayment {
 	public String upiPayment(String upi,float amount)
 	{
 		return "upipayment amount"+amount+"is done";
+	}
+}
+class UPIAmount{
+	public double enhancement(String upid)
+	{
+		return new Random().nextInt(120000);
 	}
 }
